7c1c7c1d5151c8bc30ff1594717e5445255595f3
Aparna Jha <aparnajha482@gmail.com>
2018-03-12 08:22:12 +0530

    print and read contents in link.

--- profanity_editor.py
+++ profanity_editor.py
@@ -5,8 +5,9 @@ def edit_file():
     quotes.close()
 
 def check_profanity(text_to_check):
-    urllib.urlopen("http://www.wdyl.com/profanity?q=" + text_to_check)
-
+    connection = urllib.urlopen("http://www.wdyl.com/profanity?q=" + text_to_check)
+    output = connection.read()
+    print(output)
 
 
 
