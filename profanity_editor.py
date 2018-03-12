16654b85330f9da63082f5ca5dd7bb37e22e2621
Aparna Jha <aparnajha482@gmail.com>
2018-03-12 08:24:19 +0530

    import urllib module to use functionalities of module

--- profanity_editor.py
+++ profanity_editor.py
@@ -1,13 +1,17 @@
+import urllib
+
 def edit_file():
     quotes = open("text_file/file.txt")
     contents = quotes.read()
     print(contents)
     quotes.close()
+    check_profanity(contents)
 
 def check_profanity(text_to_check):
     connection = urllib.urlopen("http://www.wdyl.com/profanity?q=" + text_to_check)
     output = connection.read()
     print(output)
+    connection.close()
 
 
 
